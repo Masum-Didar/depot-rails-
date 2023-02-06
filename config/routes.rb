68f8e278dad66603994fbd80010c18f00998582d
Abu Masum Didar <masumdidar.cse@gmail.com>
2023-02-06 14:51:50 +0600

    Atom Feeds

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   #root_path
 
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products do
+    get :who_bought, on: :member
+  end
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
 end
